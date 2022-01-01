e9d73e67fa6aa840b8bb59fcb028d3d575acfc58
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-01 10:37:31 -0300

    Instala ESLint Airbnb

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Cabeçalho</header>
+      <main>Conteúdo Principal</main>
+      <footer>rodapé</footer>
+    </div>
+  );
+}
+
+export default App;
