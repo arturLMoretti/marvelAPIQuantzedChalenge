9a95370bd26a30ea5858d4c1ad5f5581f47eb9f1
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-02 23:00:32 -0300

    Adiciona rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Characters from './pages/Characters';
 import Home from './pages/Home';
+import Comics from './pages/Comics';
+import Creators from './pages/Creators';
+import Events from './pages/Events';
+import Series from './pages/Series';
 
 export default function App() {
   return (
@@ -9,13 +13,28 @@ export default function App() {
       <Routes>
         <Route
           exact
+          path="/"
+          element={<Home />}
+        />
+        <Route
           path="/characters"
           element={<Characters />}
         />
         <Route
-          exact
-          path="/"
-          element={<Home />}
+          path="/comics"
+          element={<Comics />}
+        />
+        <Route
+          path="/creators"
+          element={<Creators />}
+        />
+        <Route
+          path="/events"
+          element={<Events />}
+        />
+        <Route
+          path="/series"
+          element={<Series />}
         />
       </Routes>
     </Router>
