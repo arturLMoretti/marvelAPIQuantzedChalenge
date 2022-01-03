13b34133b27b366e57b3c2b3321bc398c73a5e04
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-02 23:01:26 -0300

    Cria página Comics

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import { Container } from 'react-bootstrap';
+import { apiRequest } from '../services/apiRequest';
+import Header from '../components/Header';
 
-function Comics() {
+function Characters() {
+  const [comics, setComics] = useState([]);
+  useEffect(() => {
+    apiRequest('comics').then((response) => setComics(response.data.data.results));
+  }, []);
   return (
-    <h1>Comics</h1>
+    <>
+      <Header />
+      <main>
+        {comics.length > 0 ? (
+          <Container>
+            {comics.map((comic) => (
+              <Card key={comic.id} style={{ width: '20rem' }}>
+                <Card.Img variant="top" src={`${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`} />
+                <Card.Body>
+                  <Card.Title>{comic.title}</Card.Title>
+                  <Card.Text>
+                    {!comic.description ? 'Descrição indisponível!' : comic.description}
+                  </Card.Text>
+                  {comic.urls.map((url) => <Card.Link target="_blank" href={url.url}>{url.type}</Card.Link>)}
+                </Card.Body>
+              </Card>
+            ))}
+          </Container>
+        )
+          : <h1>Carregando...</h1>}
+      </main>
+      <footer>Rodapé</footer>
+    </>
   );
 }
 
-export default Comics;
+export default Characters;
