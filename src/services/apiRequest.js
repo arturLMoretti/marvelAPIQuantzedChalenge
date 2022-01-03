a633ce73f8c7946157563e47218bb2630de0e34e
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-02 23:02:35 -0300

    Exporta publicKey

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -1,14 +1,11 @@
 import md5 from 'md5';
 import axios from 'axios';
 
-const apiRequest = (requisitonType) => {
-  const publicKey = 'fbf2a60cf4cbf094dc433ca4037b89b9';
-  const privateKey = 'a0a8dd22cebe51b138111118e7a10cd66b66297a';
+export const publicKey = 'fbf2a60cf4cbf094dc433ca4037b89b9';
+const privateKey = 'a0a8dd22cebe51b138111118e7a10cd66b66297a';
 
-  const timeStamp = Number(new Date());
+const timeStamp = Number(new Date());
 
-  const hash = md5(timeStamp + privateKey + publicKey);
-  return axios.get(`http://gateway.marvel.com/v1/public/${requisitonType}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
-};
+const hash = md5(timeStamp + privateKey + publicKey);
 
-export default apiRequest;
+export const apiRequest = (requisitonType) => axios.get(`http://gateway.marvel.com/v1/public/${requisitonType}?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
