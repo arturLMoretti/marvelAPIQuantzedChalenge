3e569fef9d10a820b9f059381972e0c7a903babd
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-02 23:01:41 -0300

    Cria página Creators

--- src/pages/Creators.jsx
+++ src/pages/Creators.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Header from '../components/Header';
+import { apiRequest } from '../services/apiRequest';
 
-function Creators() {
+function Characters() {
+  const [creators, setCreators] = useState([]);
+  useEffect(() => {
+    apiRequest('creators').then((response) => setCreators(response.data.data.results));
+  }, []);
   return (
-    <h1>Creators</h1>
+    <>
+      <Header />
+      <main>
+        <Container>
+          {creators.map((creator) => (
+            <Card key={creator.id} style={{ width: '20rem' }}>
+              <Card.Header>{creator.fullName}</Card.Header>
+              {creator.urls.map((url) => <Card.Link target="_blank" href={url.url}>{url.type}</Card.Link>)}
+            </Card>
+          ))}
+        </Container>
+      </main>
+      <footer>Rodapé</footer>
+    </>
   );
 }
 
-export default Creators;
+export default Characters;
