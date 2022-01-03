63605a5ba7464e9fbd7235a35f1843a42843568b
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-03 08:18:45 -0300

    Adciona Carregando...

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -19,42 +19,44 @@ function Characters() {
           className="g-4 h-100"
           style={{ margin: 'auto 0' }}
         >
-          {characters.map((character) => (
-            <Card
-              className="d-flex align-items-stretch"
-              key={character.name}
-              style={{
-                maxWidth: '250px',
-                margin: '10px',
-                display: 'flex',
-              }}
-            >
-              <Card.Img
-                variant="top"
-                src={`${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`}
-              />
-              <Card.Body
-                className="d-flex flex-column align-items-stretch"
+          {characters.length > 0 ? (
+            characters.map((character) => (
+              <Card
+                className="d-flex align-items-stretch justify-content-between"
+                key={character.name}
+                style={{
+                  maxWidth: '250px',
+                  margin: '10px',
+                  display: 'flex',
+                }}
               >
-                <Card.Title>{character.name}</Card.Title>
-                <Card.Text
-                  style={{ textAlign: 'justify' }}
+                <Card.Img
+                  variant="top"
+                  src={`${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`}
+                />
+                <Card.Body
+                  className="d-flex flex-column align-items-stretch justify-content-between"
                 >
-                  {character.description.length === 0 ? 'Descrição indisponível!' : character.description}
-                </Card.Text>
-                <Card.Footer>
-                  {character.urls.map((url) => (
-                    <Card.Link
-                      target="_blank"
-                      href={url.url}
-                    >
-                      {url.type}
-                    </Card.Link>
-                  ))}
-                </Card.Footer>
-              </Card.Body>
-            </Card>
-          ))}
+                  <Card.Title>{character.name}</Card.Title>
+                  <Card.Text
+                    style={{ textAlign: 'justify' }}
+                  >
+                    {character.description.length === 0 ? 'Descrição indisponível!' : character.description}
+                  </Card.Text>
+                  <Card.Footer style={{ fontSize: '14px' }}>
+                    {character.urls.map((url) => (
+                      <Card.Link
+                        target="_blank"
+                        href={url.url}
+                      >
+                        {url.type}
+                      </Card.Link>
+                    ))}
+                  </Card.Footer>
+                </Card.Body>
+              </Card>
+            ))
+          ) : <h1>Carregando...</h1>}
         </Row>
       </main>
       <footer>Rodapé</footer>
