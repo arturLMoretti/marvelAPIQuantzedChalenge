e84bd005d27f2625423c634d93418c23986f15f1
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-03 08:18:20 -0300

    Altera cor do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,21 @@ import Nav from 'react-bootstrap/Nav';
 export default function Header() {
   return (
     <header>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/characters">Personagens</Nav.Link>
-            <Nav.Link href="/comics">Quadrinhos</Nav.Link>
-            <Nav.Link href="/creators">Criadores</Nav.Link>
-            <Nav.Link href="/events">Eventos</Nav.Link>
-            <Nav.Link href="/series">Séries</Nav.Link>
+            <Nav.Link className="text-white" href="/">Home</Nav.Link>
+            <Nav.Link className="text-white" href="/characters">Personagens</Nav.Link>
+            <Nav.Link className="text-white" href="/comics">Quadrinhos</Nav.Link>
+            <Nav.Link className="text-white" href="/creators">Criadores</Nav.Link>
+            <Nav.Link className="text-white" href="/events">Eventos</Nav.Link>
+            <Nav.Link className="text-white" href="/series">Séries</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
