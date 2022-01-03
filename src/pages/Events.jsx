83b00e954d24a92b32a1651aaba3a6347f881ede
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-03 08:19:15 -0300

    Adciona Carregando...

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -14,31 +14,33 @@ function Events() {
       <Header />
       <main>
         <Container>
-          {events.map((event) => (
-            <Card
-              key={event.id}
-              style={{ width: '20rem' }}
-            >
-              <Card.Img
-                variant="top"
-                src={`${event.thumbnail.path}/standard_medium.${event.thumbnail.extension}`}
-              />
-              <Card.Body>
-                <Card.Title>{event.title}</Card.Title>
-                <Card.Text>
-                  {event.description.length === 0 ? 'Descrição indisponível!' : event.description}
-                </Card.Text>
-                {event.urls.map((url) => (
-                  <Card.Link
-                    target="_blank"
-                    href={url.url}
-                  >
-                    {url.type}
-                  </Card.Link>
-                ))}
-              </Card.Body>
-            </Card>
-          ))}
+          {events.length > 0 ? (
+            events.map((event) => (
+              <Card
+                key={event.id}
+                style={{ width: '20rem' }}
+              >
+                <Card.Img
+                  variant="top"
+                  src={`${event.thumbnail.path}/standard_fantastic.${event.thumbnail.extension}`}
+                />
+                <Card.Body>
+                  <Card.Title>{event.title}</Card.Title>
+                  <Card.Text>
+                    {event.description.length === 0 ? 'Descrição indisponível!' : event.description}
+                  </Card.Text>
+                  {event.urls.map((url) => (
+                    <Card.Link
+                      target="_blank"
+                      href={url.url}
+                    >
+                      {url.type}
+                    </Card.Link>
+                  ))}
+                </Card.Body>
+              </Card>
+            ))
+          ) : <h1>Carregando...</h1>}
         </Container>
       </main>
       <footer>Rodapé</footer>
