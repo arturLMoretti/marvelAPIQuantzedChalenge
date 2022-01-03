77a17aaa04e22bd09cc9703d356779619347d99a
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-03 10:14:17 -0300

    Corrige BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Characters from './pages/Characters';
 import Home from './pages/Home';
 import Comics from './pages/Comics';
@@ -9,34 +9,32 @@ import Series from './pages/Series';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={<Home />}
-        />
-        <Route
-          path="/characters"
-          element={<Characters />}
-        />
-        <Route
-          path="/comics"
-          element={<Comics />}
-        />
-        <Route
-          path="/creators"
-          element={<Creators />}
-        />
-        <Route
-          path="/events"
-          element={<Events />}
-        />
-        <Route
-          path="/series"
-          element={<Series />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/characters"
+        element={<Characters />}
+      />
+      <Route
+        path="/comics"
+        element={<Comics />}
+      />
+      <Route
+        path="/creators"
+        element={<Creators />}
+      />
+      <Route
+        path="/events"
+        element={<Events />}
+      />
+      <Route
+        path="/series"
+        element={<Series />}
+      />
+    </Routes>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
