c75e04e587468b8960b16a5a1bd33da3744d03f5
Artur Lemes Moretti <arturl.moretti@gmail.com>
2022-01-02 23:01:11 -0300

    Adicioa Header

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import apiRequest from '../services/apiRequest';
+import { Row } from 'react-bootstrap';
+import { apiRequest } from '../services/apiRequest';
+import Header from '../components/Header';
 
 function Characters() {
   const [characters, setCharacters] = useState([]);
@@ -9,24 +10,55 @@ function Characters() {
     apiRequest('characters').then((response) => setCharacters(response.data.data.results));
   }, []);
   return (
-    characters.map((character) => (
-      <Card key={character.name} style={{ width: '20rem' }}>
-        <Card.Img variant="top" src={`${character.thumbnail.path}/standard_medium.${character.thumbnail.extension}`} />
-        <Card.Body>
-          <Card.Title>{character.name}</Card.Title>
-          <Card.Text>
-            {character.description.length === 0 ? 'Descrição indisponível!' : character.description}
-          </Card.Text>
-          <div className="button-container">
-            <Button variant="primary">Revistas</Button>
-            <Button variant="primary">Eventos</Button>
-            <Button variant="primary">Séries</Button>
-            <Button variant="primary">Histórias</Button>
-          </div>
-          {character.urls.map((url) => <Card.Link target="_blank" href={url.url}>{url.type}</Card.Link>)}
-        </Card.Body>
-      </Card>
-    ))
+    <>
+      <Header />
+      <main>
+        <Row
+          xs={1}
+          md={2}
+          className="g-4 h-100"
+          style={{ margin: 'auto 0' }}
+        >
+          {characters.map((character) => (
+            <Card
+              className="d-flex align-items-stretch"
+              key={character.name}
+              style={{
+                maxWidth: '250px',
+                margin: '10px',
+                display: 'flex',
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={`${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`}
+              />
+              <Card.Body
+                className="d-flex flex-column align-items-stretch"
+              >
+                <Card.Title>{character.name}</Card.Title>
+                <Card.Text
+                  style={{ textAlign: 'justify' }}
+                >
+                  {character.description.length === 0 ? 'Descrição indisponível!' : character.description}
+                </Card.Text>
+                <Card.Footer>
+                  {character.urls.map((url) => (
+                    <Card.Link
+                      target="_blank"
+                      href={url.url}
+                    >
+                      {url.type}
+                    </Card.Link>
+                  ))}
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </main>
+      <footer>Rodapé</footer>
+    </>
   );
 }
 
